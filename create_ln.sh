124d1f287fcec2fca573d5e4a0b2b8d5c7ba0a0b
carlos <caedo.00@gmail.com>
2015-07-24 13:54:09 -0600

    Show hidden files.

--- create_ln.sh
+++ create_ln.sh
@@ -1,9 +1,9 @@
 rm ~/.gvimrc
 rm ~/.bashrc
 rm ~/.zshrc
-ln -s ~/dotfiles/.gvimrc ~/.gvimrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/gvimrc ~/.gvimrc
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
 # OPENBOX
 rm -R ~/.config/openbox
 mkdir ~/.config/openbox
@@ -16,4 +16,4 @@ ln -s ~/dotfiles/tint2/tint2rc ~/.config/tint2/tint2rc
 ln -s ~/dotfiles/tint2/tintwizard.conf ~/.config/tint2/tintwizard.conf
 # VIM COLORSCHEME
 rm ~/.vim/colors/gruvbox.vim
-ln -s ~/dotfiles/.vim/colors/gruvbox.vim ~/.vim/colors/gruvbox.vim
+ln -s ~/dotfiles/vim/ ~/.vim/colors/gruvbox.vim
