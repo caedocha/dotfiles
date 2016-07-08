219b097aafca8a1464eb9a7c537e4de56efa026d
Carlos <caedo.00@gmail.com>
2016-07-07 21:18:51 -0600

    Remove tint and openbox config

--- openbox/autostart.sh
+++
@@ -1,6 +0,0 @@
-xrandr --auto --output DFP10 --mode 1920x1080 --right-of DFP9 &
-xrandr --auto --output DFP9 --mode 1920x1080 --left-of DFP10 &
-tint2 &
-nitrogen --restore &
-pytyle2 &
-setxkbmap -option caps:none &
