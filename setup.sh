d0d4884c0710916992447cfbf94b4250a17a7fcc
Carlos Chacon <caedo.00@gmail.com>
2017-12-23 16:03:49 -0600

    Set useHttpPath in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+echo 'SETTING GIT CONFIG...'
+git config --global credential.useHttpPath true
+echo 'FINISHED'
+
 echo 'CREATING SYMLINKS:'
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
