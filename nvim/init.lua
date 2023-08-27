4eb2325ff7697d4f0e02159bb00e250fa11f4df8
Carlos Chacon <caedo.00@gmail.com>
2023-08-27 11:59:18 -0600

    Add treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,9 @@ set.tabstop = 2
 set.shiftwidth = 2
 set.softtabstop = 2
 set.expandtab = true
+set.foldmethod = "expr"
+set.foldexpr = "nvim_treesitter#foldexpr()"
+set.foldenable = false
 vim.g.mapleader = ","
 
 local ls = require("luasnip")
@@ -21,7 +24,7 @@ end, {silent = true})
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
-return require("packer").startup(function()
+require("packer").startup(function()
   use 'wbthomason/packer.nvim'
   use 'ggandor/leap.nvim'
   use "rafamadriz/friendly-snippets"
@@ -32,4 +35,26 @@ return require("packer").startup(function()
     -- install jsregexp (optional!:).
     run = "make install_jsregexp"
   })
+  use({
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end
+  })
 end)
+
+local ts = require('nvim-treesitter.configs')
+ts.setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "c_sharp", "ruby", "svelte" },
+  sync_install = true,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true,
+  }
+})
+
