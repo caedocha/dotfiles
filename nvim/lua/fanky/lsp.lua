437246f6435ab62a84cf2e9f6b4a8f66f78a0cf6
Carlos <caedo.00@gmail.com>
2025-11-22 16:19:13 -0600

    Add Lua ls

--- nvim/lua/fanky/lsp.lua
+++ nvim/lua/fanky/lsp.lua
@@ -3,5 +3,9 @@ config = {
     print("This will run when the server attaches!")
   end,
 }
+
 vim.lsp.config('roslyn', config)
 vim.lsp.enable('roslyn')
+
+vim.lsp.config('lua_ls', config)
+vim.lsp.enable('lua_ls')
